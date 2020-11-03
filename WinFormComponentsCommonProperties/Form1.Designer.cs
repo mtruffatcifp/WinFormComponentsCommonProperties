a63b4605a5dcc81bc5b9aae325c39eabd3571e69
Marco <marcotruffat@paucasesnovescifp.cat>
2020-11-03 23:36:40 +0100

    Created project for WinFormComponentsCommonProperties.

---
+++ WinFormComponentsCommonProperties/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WinFormComponentsCommonProperties
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.targetButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.formSizeLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // targetButton
+            // 
+            this.targetButton.Location = new System.Drawing.Point(0, 0);
+            this.targetButton.Name = "targetButton";
+            this.targetButton.Size = new System.Drawing.Size(100, 100);
+            this.targetButton.TabIndex = 0;
+            this.targetButton.Text = "Target\r\nX = 0\r\nY = 0\r\n";
+            this.targetButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(570, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 500);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controls";
+            // 
+            // formSizeLabel
+            // 
+            this.formSizeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.formSizeLabel.AutoSize = true;
+            this.formSizeLabel.Location = new System.Drawing.Point(20, 530);
+            this.formSizeLabel.Name = "formSizeLabel";
+            this.formSizeLabel.Size = new System.Drawing.Size(35, 13);
+            this.formSizeLabel.TabIndex = 2;
+            this.formSizeLabel.Text = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Fixed3D",
+            "FixedDialog",
+            "FixedSingle",
+            "FixedToolWindow",
+            "None",
+            "Sizable",
+            "SizableToolWindow\t"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(188, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.formSizeLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.targetButton);
+            this.Name = "mainForm";
+            this.Text = "(0, 0) Winforms Components - Common Controls";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.Move += new System.EventHandler(this.mainForm_Move);
+            this.Resize += new System.EventHandler(this.mainForm_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button targetButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label formSizeLabel;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WinFormComponentsCommonProperties/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormComponentsCommonProperties
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            formSizeLabel.Text = $"Form Size: {this.Width}, {this.Height}";
+            this.Text = $"({this.DesktopLocation.X.ToString()}, {this.DesktopLocation.Y.ToString()}) Winforms Components - Common Controls";
+        }
+        private void mainForm_Move(object sender, EventArgs e)
+        {
+            this.Text = $"({this.DesktopLocation.X.ToString()}, {this.DesktopLocation.Y.ToString()}) Winforms Components - Common Controls";
+        }
+
+        private void mainForm_Resize(object sender, EventArgs e)
+        {
+            formSizeLabel.Text = $"Form Size: {this.Width}, {this.Height}";
+        }
+    }
+}
